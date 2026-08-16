72301a66768699ea1cc28a8fb2c6a77a38e14c2e
Kartikey-Kesarwani-2005 <kesarwanikartikey786@gmail.com>
2026-08-16 21:55:33 +0530

    Add Variable File

---
+++ Variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456789;
+let accountName = "John Doe";
+let accountBalance = 1000.50; 
+accountNominee = "Doe";
+accountBalance = 12000;
+accountNominee = "Jane";
+accountemail = "jane.doe@example.com"
+let accountStatus;
+// accountId = 987654321; // This will throw an error because accountId is a constant and cannot be reassigned.
+console.log("Account ID:", accountId);
+console.table([accountId, accountName, accountBalance, accountNominee, accountemail, accountStatus]);
