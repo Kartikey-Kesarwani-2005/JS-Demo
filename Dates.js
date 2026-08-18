6a5888d8088e59bf4e0243b1c8566d35550885c5
Kartikey-Kesarwani-2005 <kesarwanikartikey786@gmail.com>
2026-08-18 17:32:19 +0530

    Add Dates Page

---
+++ Dates.js
@@ -0,0 +1,17 @@
+// let MyDate = new Date();
+// console.log(MyDate);
+// console.log(MyDate.getDay());
+// console.log(MyDate.getFullYear());
+// console.log(MyDate.getMonth());
+// console.log(MyDate.toString());
+// console.log(MyDate.toTimeString());
+// console.log(MyDate.toDateString());
+// console.log(MyDate.toISOString());
+// console.log(MyDate.toJSON());
+// console.log(MyDate.toLocaleString());
+
+
+
+let NewDate = new Date(2023, 5, 15, 10, 30, 0);
+console.log(NewDate);
+console.log(NewDate.getDay());  
