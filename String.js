871479e7a2412af2e6a4a739b4a3ebd206523dff
Kartikey-Kesarwani-2005 <kesarwanikartikey786@gmail.com>
2026-08-18 16:26:24 +0530

    Add String Part

---
+++ String.js
@@ -0,0 +1,21 @@
+const name = "Kartikey";
+const Repo = "Java_Script";
+console.log(name + Repo + "Demo");
+console.log(`My Name Is ${name} . I am Creating Repo And RepoName Is ${Repo}`);
+
+const Str = new String("Hello World");
+console.log(Str);
+console.log(Str.length);
+console.log(Str.toUpperCase());
+console.log(Str[2]);
+console.log(Str.indexOf("o"));
+console.log(Str.charAt(2));
+
+const newString = Str.substring(0, 6);
+console.log(newString); 
+const anotherString = Str.slice(-9, 6);
+console.log(anotherString); 
+
+const String1 = "         KArtikey         ";
+console.log(String1);
+console.log(String1.trim());
