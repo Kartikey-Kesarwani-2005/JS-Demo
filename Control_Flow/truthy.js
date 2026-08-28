29b3a1af8f71c1304634757fd01083c7a4da5acf
Kartikey-Kesarwani-2005 <kesarwanikartikey786@gmail.com>
2026-08-28 20:12:52 +0530

    Add Truthy and Falsy and some Operator

---
+++ Control_Flow/truthy.js
@@ -0,0 +1,48 @@
+// const UserEmail = "Abhay@Opencode";
+// if(UserEmail){
+//     console.log("User Find Email");
+// }
+// else{
+//     console.log("Con't find Email");
+// }
+
+
+// // Falsy value
+// const UserEmail1 = "";
+// if(UserEmail1){
+//     console.log("User Find Email");
+// }
+// else{   
+//     console.log("Con't find Email");
+// }
+
+// const Array = [];
+// if(Array.length === 0){
+//     console.log("Array is Empty");
+// }
+
+// const object = {};
+// if(Object.keys(object).length === 0){
+//     console.log("Object is Empty");
+// }
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// nullish coalescing Operation (??) : null undefined
+
+// let val1;
+// val1 = 10 ?? 52;
+// val1 = null ?? 52;
+// val1 = undefined ?? 23;
+// val1 = null ?? 12 ?? 13;
+// console.log(val1);
+
+
+// Terniary Operator (?)
+// condition ? true : false
+
+const Mark = 90;
+Mark >=80 ? console.log("Greater then 80") : console.log("Less then 80");
+
