5e29ac5169eb4b0cf5c1f4646c9595c308aaca1c
Kartikey-Kesarwani-2005 <kesarwanikartikey786@gmail.com>
2026-08-28 21:28:17 +0530

    Add basic Loops

---
+++ Iteration/loops.js
@@ -0,0 +1,33 @@
+// for(let i=0;i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+
+// for(let i = 1;i<=10;i++){
+//     console.log(`Outer Loop ${i}`);
+//     for(let j = 1; j<=10 ; j++){
+//        // console.log(`Inner Loop ${j}`);
+//        console.log(`${i}  * ${j}  = ${i*j}`);
+//     }
+// }
+
+// const MyArray = ["Suraj","Saurabh","Vaibhav"]
+// for (let index = 0; index < MyArray.length; index++) {
+//     const element = MyArray[index];
+//     console.log(element)
+// }
+
+
+// let i=0;
+// while(i<=22){
+//     console.log(i);
+//     i=i+1;
+// }
+
+
+let i = 10;
+do {
+    console.log(i);
+    i--;
+}while(i>=10);
