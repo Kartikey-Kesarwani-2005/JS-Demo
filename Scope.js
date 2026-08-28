3b145987434090cc5da6a578d80364209ff3629b
Kartikey-Kesarwani-2005 <kesarwanikartikey786@gmail.com>
2026-08-28 17:54:52 +0530

    Add Scope File

---
+++ Scope.js
@@ -0,0 +1,39 @@
+function One(){
+    const name = "Abhay";
+    
+    function Two(){
+        const Age = 22;
+        console.log(name);
+
+    }
+    //console.log(Age);
+    Two();
+}
+// One();
+
+
+// if(true){
+//     const Username = "Abhay";
+//     if(Username == "Abhay"){
+//         const Website = " Youtube";
+//         console.log(Username + Website);
+//     }
+    // console.log(Website);
+// }
+// console.log(Username);
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function addOne(num){
+    return num + 1 ;
+}
+console.log(addOne(5));
+
+
+const addnum = function(value){
+    return value + 2;
+}
+console.log(addnum(5));
